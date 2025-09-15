72d1ccc71b74cd56bab62b2ffdaae34b137d4766
[dyad] <stefandjvermeulen@gmail.com>
2025-09-15 18:07:06 +0200

    [dyad] Fixed image loading error by updating Next.js config. - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "api.dicebear.com",
       },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
     ],
   },
   webpack: (config) => {
